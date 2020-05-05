0b31ee52f860526689e8f26709e5b6ddd3fe4478
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-05-05 15:58:17 +0000

    conflit result

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -127,7 +127,7 @@
 {{--    src="https://code.jquery.com/jquery-2.2.4.js"--}}
 {{--    integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="--}}
 {{--    crossorigin="anonymous"></script>--}}
-<script src="js/jquery.fancybox.min.js"></script>
+<script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
 
 @yield('script')
 </body>
