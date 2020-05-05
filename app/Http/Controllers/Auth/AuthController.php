0ddeeadddce4cbe232b9c459d4f2b0078c799669
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-05-05 15:42:51 +0000

    conflit result

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -133,4 +133,18 @@ class AuthController extends Controller
         ]);
 
     }
+
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/dashboard';
+    }
 }
