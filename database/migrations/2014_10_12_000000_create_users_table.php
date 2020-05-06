b57c56c1f7c2ea5ad61091d5c2c71ad017836877
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-05-06 12:23:57 +0000

    conflit result

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('country');
             $table->string('city');
             $table->string('address');
