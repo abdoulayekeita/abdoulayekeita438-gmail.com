dabc065e809eadafbf24c96831101094028e66c8
abdoulaye93 <abdoulayekeita438@gmail.com>
2020-09-28 17:07:49 +0000

    test

--- app/Http/Controllers/Dashboard/ImageUploadController.php
+++ app/Http/Controllers/Dashboard/ImageUploadController.php
@@ -26,7 +26,7 @@ class ImageUploadController extends Controller
             $image_64 = base64_encode(file_get_contents($request->file('file')->getPathname()));
             $extension = $request->file('file')->getClientOriginalExtension();
             $imageName = Str::random(20).'.'.$extension;
-//            Redis::set($imageName, $image_64);
+            Redis::set($imageName, $image_64);
             echo json_encode($imageName);
         }
     }
